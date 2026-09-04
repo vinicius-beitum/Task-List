e0e93c29ed7abdb5d0516a884f3478603c06e628
Nickluzlains-Nll <nick.luzlains@gmail.com>
2026-09-04 20:05:49 -0300

    Update login.js
    finalização da parte de lembrar senha

--- login/login.js
+++ login/login.js
@@ -1,11 +1,25 @@
 const formulario = document.getElementById("loginForm");
 
+     const email = document.getElementById("email");
+     const senha = document.getElementById("senha");
+     const lembrar = document.getElementById("lembrar");
+
+window.addEventListener("load", function() {
+
+   const emailSalvo = locolStorage.getItem("email");
+   if(emailSalvo) {
+      emailInput.value = emailSalvo;
+      lembrar.checked = true;
+   }
+});
+
+
 formulario.addEventListener("submit", function(event) {
 
      event.preventDefault();
 
-     const email = document.getElementById("email").value;
-     const senha = document.getElementById("senha").value;
+     const email = email = emailInput.value;
+     const senha = senhaInput.value;
 
 // verificação de email e senha
      
@@ -53,18 +67,42 @@ if (!/[!@#$%^&*(),.?":{}|<>_\-+=]/.test(senha)) {
     return;
 }
 
+if (lembrar.checked) {
+   localStorage.setItem("email", email);
+} else {
+   localStorage.removeItem("email");
+}
+
+
      window.location.href = "../home/home.html";
 });
 
+// Mostrar e ocultar senha
+
 const mostrarSenha = document.getElementById("mostrarSenha");
-const senha = document.getElementById("senha");
+
+mostrarSenha.addEventListener("click", function() {
+   if(senhaInput.type === "password") {
+      senhaInput.type = "text";
+      mostrarSenha.classList.remove("fa-eye");
+      mostrarSenha.classList.add("fa-eye-slash");
+
+   }else{
+      
+      senhaInput.type = "password";
+      mostrarSenha.classList.remove("fa-eye-slash");
+      mostrarSenha.classList.add("fa-eye");
+   }
+});
 
 mostrarSenha.addEventListener("click",function() {
    if(senha.type === "password") {
       senha.type = "text";
       mostrarSenha.clssList.remove("fa-eye");
       mostrarSenha.classList.add("fa-eye-slash");
-      }else{
+
+   } else {
+
       senha.type = "password";
       mostrarSenha.classList.remove("fa-eye-slash");
       mostrarSenha.classList.add("fa-eye");
