476a5a07a03b4583d37b315ffa4680c374b95006
Vinicius Beitum <beitum.vinicius2010@gmail.com>
2026-09-04 20:43:05 -0300

    Refatoração do código

--- login/login.js
+++ login/login.js
@@ -3,12 +3,13 @@ const formulario = document.getElementById("loginForm");
      const email = document.getElementById("email");
      const senha = document.getElementById("senha");
      const lembrar = document.getElementById("lembrar");
+     const mostrarSenha = document.getElementById("mostrarSenha");
 
 window.addEventListener("load", function() {
 
-   const emailSalvo = locolStorage.getItem("email");
+   const emailSalvo = localStorage.getItem("email");
    if(emailSalvo) {
-      emailInput.value = emailSalvo;
+      email.value = emailSalvo;
       lembrar.checked = true;
    }
 });
@@ -18,8 +19,8 @@ formulario.addEventListener("submit", function(event) {
 
      event.preventDefault();
 
-     const email = email = emailInput.value;
-     const senha = senhaInput.value;
+     const emailDigitado = email.value
+     const senhaDigitada = senha.value
 
 // verificação de email e senha
      
@@ -93,18 +94,4 @@ mostrarSenha.addEventListener("click", function() {
       mostrarSenha.classList.remove("fa-eye-slash");
       mostrarSenha.classList.add("fa-eye");
    }
-});
-
-mostrarSenha.addEventListener("click",function() {
-   if(senha.type === "password") {
-      senha.type = "text";
-      mostrarSenha.clssList.remove("fa-eye");
-      mostrarSenha.classList.add("fa-eye-slash");
-
-   } else {
-
-      senha.type = "password";
-      mostrarSenha.classList.remove("fa-eye-slash");
-      mostrarSenha.classList.add("fa-eye");
-      }
 });
